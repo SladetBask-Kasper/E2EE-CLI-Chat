f2d6a2621fcbfe32287a6e39db50aae16c1849bd
Kasper Evertsson <kasper4131@gmail.com>
2020-11-09 16:35:25 +0100

    Removed old crap

--- encryption.py
+++ encryption.py
@@ -3,12 +3,6 @@ from Crypto import Random
 from Crypto.Cipher import AES
 from settings import *
 
-def printKey(key, title = ""):
-	putTitle = ""
-	if len(title) > 0:
-		putTitle = f" ({title})"
-	print(f"{putTitle} [{len(key)}] {key}.\r")
-
 #
 # Class originally taken from : https://stackoverflow.com/a/21928790
 #
@@ -57,6 +51,10 @@ class AESCipher(object):
 	def getKey(self):
 		return "("+ str(self.keylen)+") " + self.hexkey
 
+#
+# =========================================================================
+#
+
 alwayCryptor = AESCipher(ALWAYS_USE_KEY)
 def pack(msg):
 	global alwayCryptor
@@ -71,9 +69,6 @@ def cls():
 # secure send
 def ssend(socket, message, aes):
 	data = gzip.compress(aes.encrypt(pack(message)))
-	"""if len(str(data.decode())) >= 32:
-		print("Compressing message")
-		data = b'@gzip' + gzip.compress(data)"""
 	socket.sendall(data)
 
 def receivedata(socket, aes):
@@ -85,50 +80,4 @@ def receivedata(socket, aes):
 		exit()
 	if not data:
 		return None
-	"""if data[:5] == b"@gzip":
-		print("Decompressing message")
-		data =  gzip.decompress(data[5:])"""
 	return str(unpack(aes.decrypt(gzip.decompress(data))))
-
-"""if __name__ == "__main__":
-	data = "This is very secret."
-	printKey(data, "Original msg")
-	aes = AESCipher()
-	printKey(aes.getKey(), "Key")
-	enc_data = aes.encrypt(data)
-	printKey(str(enc_data), "Encrypted data")
-	#print (enc_data == enc_data.decode().encode())
-	#printKey(str(base64.b64decode(enc_data)), "Decoded encrypted data")
-	dec_data = aes.decrypt(enc_data)
-	printKey('"'+str(dec_data)+'"', "Decrypted data")"""
-if __name__ == "__main__":
-	data = "This is very secret."
-	#data = "a"*530 # double encrypted data is 1024 chars long
-	#data = "a"*1110 # double encrypted data is 2048 chars long
-	#printKey(data, "Original msg")
-	aes = AESCipher()
-	enc_data = aes.encrypt(data)
-	printKey(str(enc_data.decode()), "Encrypted data")
-	a2 = AESCipher()
-	double_enc_data = a2.encrypt(enc_data.decode())
-	printKey(str(double_enc_data.decode()), "Double Encrypted Data")
-
-	dec1 = a2.decrypt(double_enc_data).encode()
-	dec2 = aes.decrypt(dec1)
-
-
-	#dec_data = aes.decrypt(enc_data)
-	#printKey('"'+str(dec2)+'"', "Decrypted data")
-
-"""
-data = "This is very secret."
-
-aes = AESCipher()
-enc_data = aes.encrypt(data)
-
-a2 = AESCipher()
-double_enc_data = a2.encrypt(enc_data.decode())
-
-dec1 = a2.decrypt(double_enc_data).encode()
-dec2 = aes.decrypt(dec1) # done
-"""
