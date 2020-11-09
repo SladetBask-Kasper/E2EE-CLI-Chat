31c1cc6a7a14bd0b92150cba2d76fbf8fc1f78db
Kasper <kasper4131@gmail.com>
2020-11-09 07:27:05 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,131 @@
+# source : https://realpython.com/python-sockets/
+import socket, threading, pyDH, gzip
+from encryption import *
+from sys import argv
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+sharedKey = ""
+diffieAES = AESCipher(ALWAYS_SAME_KEY)
+del ALWAYS_SAME_KEY
+
+if len(argv) > 1:
+	# This means arg was passed. Assuming its IP
+	HOST = str(argv[1])
+
+if len(argv) > 2:
+    # this meanse two args was passed. Assuming it's port.
+    try:
+        PORT = int(argv[2])
+    except:
+        print("Invalid port as argv. " + str(PORT) + " is port.")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+connected = True
+print(f"Connected to {HOST}:{PORT}!")
+aes = None
+# Diffie-Hellaman Key exchange 
+def difhel():
+	global aes 
+	global diffieAES
+	global s
+	global sharedKey
+
+	dh = pyDH.DiffieHellman() # Exchanger for Diffie-Hellman key exchange
+	pubkey = str(dh.gen_public_key()) # Our public key for this conversation
+	
+	print(" [*] Setting up keys...")
+
+	s.sendall(gzip.compress(diffieAES.encrypt(pack(pubkey))))
+	while True:
+		data = s.recv(MAX_PACK_LEN)
+		if not data:
+			s.close()
+			print("Failed key exchange")
+			exit()
+		# generate shared key with "data"
+		try:
+			sharedKey = dh.gen_shared_key(int(unpack(diffieAES.decrypt(gzip.decompress(data)))))
+			break
+		except gzip.BadGzipFile:
+			got = ""
+			try:
+				got = str(unpack(aes.decrypt(data)))
+			except:
+				got = str(data)
+			if not got == "changekey":
+				print("Failed key exchange, unknown message: ", got)
+				s.close()
+				exit()
+			continue
+	aes = AESCipher(sharedKey)
+	#del diffieAES
+	#print(" [*] Shared key", sharedKey)
+difhel()
+
+def listen():
+	global s
+	global connected
+
+	inDiffie = False
+	while connected:
+		data = receivedata(s, aes)
+
+		if not data:
+			connected = False
+			break
+		if data == 'disconnect':
+			safeExit()
+		elif data == "changekey":
+			print(" [*] Changing keys")
+			ssend(s, data, aes)
+			difhel()
+		else:
+			print("Server:", data)
+def sender():
+	global s
+	global connected
+
+	while connected:
+		try : 
+			msg = str(input()).strip()
+		except EOFError: 
+			safeExit()
+
+		if msg == "":
+			continue 
+		elif len(msg) > 530:
+			print("ERROR: Your message was too big to send.")
+			continue
+		elif msg == "disconnect":
+			safeExit()
+		elif msg=="clear" or msg=="cls":
+			cls()
+		else:
+			#s.sendall(aes.encrypt(pack(msg)))
+			ssend(s, msg, aes)
+t = threading.Thread(target=sender)
+t.daemon = True
+
+def safeExit():
+    global s
+    global connected
+    #global t
+
+    #t.kill() # thread will be closed upon exit
+    connected = False
+    try:
+        #s.sendall(b'disconnect')
+        ssend(ssend(s, "disconnect", aes))
+    except:
+        pass
+    s.close()
+    exit()
+
+try:
+	t.start()
+	listen()
+except:
+	safeExit()
+s.close()
---
+++ encryption.py
@@ -0,0 +1,128 @@
+# https://pypi.org/project/pycryptodome/
+import string, random, base64, hashlib, os
+from Crypto import Random
+from Crypto.Cipher import AES
+from settings import *
+
+def printKey(key, title = ""):
+	putTitle = ""
+	if len(title) > 0:
+		putTitle = f" ({title})"
+	print(f"{putTitle} [{len(key)}] {key}.\r")
+
+#
+# Class originally taken from : https://stackoverflow.com/a/21928790
+#
+class AESCipher(object):
+
+	# gets Random Alphanumeric String
+	@staticmethod
+	def genKey(length = 1024):
+		if length <= 32:
+			return ''.join(random.sample(string.ascii_letters + string.digits, length))
+		else:
+			rv = ""
+			for i in range(int(length/32)):
+				rv += ''.join(random.sample(string.ascii_letters + string.digits, 32))
+			return rv
+
+	def __init__(self, key = "None"):
+		key = str(key)
+		if (key == "None"): 
+			#key = self.genKey(32768) # 2048*16
+			key = self.genKey(2048)
+			#print("key: ", key)
+		self.bs = AES.block_size
+		self.key = hashlib.sha256(key.encode()).digest()
+		self.hexkey = hashlib.sha256(key.encode()).hexdigest()
+		self.keylen = len(key)
+
+	def encrypt(self, raw):
+		raw = self._pad(raw)
+		iv = Random.new().read(AES.block_size)
+		cipher = AES.new(self.key, AES.MODE_CBC, iv)
+		return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+
+	def decrypt(self, enc):
+		enc = base64.b64decode(enc)
+		iv = enc[:AES.block_size]
+		cipher = AES.new(self.key, AES.MODE_CBC, iv)
+		return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+	def _pad(self, s):
+		return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+	@staticmethod
+	def _unpad(s):
+		return s[:-ord(s[len(s)-1:])]
+	def getKey(self):
+		return "("+ str(self.keylen)+") " + self.hexkey
+
+alwayCryptor = AESCipher(ALWAYS_USE_KEY)
+def pack(msg):
+	global alwayCryptor
+	rv = alwayCryptor.encrypt(msg).decode()
+	return rv
+def unpack(double_enc_data):
+	global alwayCryptor
+	rv = alwayCryptor.decrypt(double_enc_data)#.encode()
+	return rv
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+# secure send
+def ssend(socket, message, aes):
+	socket.sendall(aes.encrypt(pack(message)))
+
+def receivedata(socket, aes):
+	data = None
+	try:
+		data = socket.recv(MAX_PACK_LEN)
+	except ConnectionAbortedError:
+		socket.close()
+		exit()
+	if not data:
+		return None
+	return str(unpack(aes.decrypt(data)))
+
+"""if __name__ == "__main__":
+	data = "This is very secret."
+	printKey(data, "Original msg")
+	aes = AESCipher()
+	printKey(aes.getKey(), "Key")
+	enc_data = aes.encrypt(data)
+	printKey(str(enc_data), "Encrypted data")
+	#print (enc_data == enc_data.decode().encode())
+	#printKey(str(base64.b64decode(enc_data)), "Decoded encrypted data")
+	dec_data = aes.decrypt(enc_data)
+	printKey('"'+str(dec_data)+'"', "Decrypted data")"""
+if __name__ == "__main__":
+	data = "This is very secret."
+	#data = "a"*530 # double encrypted data is 1024 chars long
+	#data = "a"*1110 # double encrypted data is 2048 chars long
+	#printKey(data, "Original msg")
+	aes = AESCipher()
+	enc_data = aes.encrypt(data)
+	printKey(str(enc_data.decode()), "Encrypted data")
+	a2 = AESCipher()
+	double_enc_data = a2.encrypt(enc_data.decode())
+	printKey(str(double_enc_data.decode()), "Double Encrypted Data")
+
+	dec1 = a2.decrypt(double_enc_data).encode()
+	dec2 = aes.decrypt(dec1)
+
+
+	#dec_data = aes.decrypt(enc_data)
+	#printKey('"'+str(dec2)+'"', "Decrypted data")
+
+"""
+data = "This is very secret."
+
+aes = AESCipher()
+enc_data = aes.encrypt(data)
+
+a2 = AESCipher()
+double_enc_data = a2.encrypt(enc_data.decode())
+
+dec1 = a2.decrypt(double_enc_data).encode()
+dec2 = aes.decrypt(dec1) # done
+"""
