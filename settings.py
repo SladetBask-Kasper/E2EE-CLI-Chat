82f38614e593937388d7565864559fa7d8debd67
Kasper Evertsson <kasper4131@gmail.com>
2020-11-14 00:24:27 +0100

    Added requirements file

--- settings.py
+++ settings.py
@@ -1,5 +1,3 @@
-# pip install pycryptodome
-# pip install pyDH
 ALWAYS_SAME_KEY = "pMKW1dmG3w4b8xjD20VgtBCTeoEsNYiv3ZfnNGqavy1VdUAeS8t5kwbuoCEXDRP7WuxCDSaz4lZgheq1EOMcUHABL9ybYvmnOY2AXMyKZudf9cIE3BozSPhl1n6Vpea8"
 ALWAYS_USE_KEY  = "r3RwNvsRmgmZM6GNCtUUQH7eQmQNGBQuaNf7U2uwxkbRbc279uc6hmhR2YgkPUnU"
 MAX_PACK_LEN    = 2048
